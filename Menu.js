61e5f8ce86288478c09f5c40a80d430edcc34d76
Thomas Sablattnig <thomas.sabe@gmx.at>
2016-03-24 07:48:46 +0000

    Create Menu and DefaulLevel

---
+++ Menu.js
@@ -0,0 +1,25 @@
+
+var Menu = {}
+
+Menu = function(){
+    
+};
+
+
+
+Menu.prototype = {
+        
+      
+        
+}
+Menu.prototype.preload = function() {
+
+};
+    
+Menu.prototype.create = function () {
+    
+};
+    
+Menu.prototype.update = function () {
+  
+};
